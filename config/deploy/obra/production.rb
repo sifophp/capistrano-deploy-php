d8472f1c683916f34cd83d75d0e3bec7b9c87f3e
Albert Lombarte <alombarte@gmail.com>
2015-03-26 22:26:04 +0100

    Deleted example deployments of live sites

--- config/deploy/obra/production.rb
+++
@@ -1,8 +0,0 @@
-# All the configuration is in "deploy.rb". We only overwrite what is needed here.
-
-# Production environment uses the "master" branch of the repository. Use it if no environment var is present
-set :branch, ENV["REVISION"] || ENV["BRANCH"] || "master"
-set :repo_url, proc { "git@bitbucket.org:alombarte/#{fetch(:application)}.git" }
-
-# Disable composer:
-Rake::Task['deploy:updated'].prerequisites.delete('composer:install')
